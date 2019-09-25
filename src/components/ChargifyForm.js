46fb364057b679da898c98dc44326ee7e008e890
nestorsalinas88 <nestorasm88@gmail.com>
2019-09-25 15:37:06 -0500

    checking proper GIT configs, need my green dots

--- src/components/ChargifyForm.js
+++ src/components/ChargifyForm.js
@@ -1,165 +1,178 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 class ChargifyForm extends Component {
-  constructor(props) {
-    super(props);
-   
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.chargifyForm = React.createRef();
-    this.state = {token: ''};
-  }
+    constructor(props) {
+        super(props);
 
-  handleSubmit(e) {
-    e.preventDefault();
-
-    this.chargify.token(
-      this.chargifyForm.current,
-
-      (token) => {
-        console.log('{host} token SUCCESS - token: ', token);
-        this.setState({token: token});
-      },
-
-      (error) => {
-        console.log('{host} token ERROR - err: ', error);
-      }
-    );
-  }
-      componentDidMount() {
-    this.chargify = new window.Chargify();
-
-    this.chargify.load({
-      // selector where the iframe will be included in the host's HTML (i.e. '#chargify-form')
-      // optional if you have a `selector` on each and every field
-      selector: '#chargify-form',
-  
-
-
-      // required label/translation (i.e. '*') for required fields
-      // Especially useful if you use 'fields' option
-      requiredLabel: '*',
-
-      firstName: {
-        // selector, where the iframe with this field will be included
-        // on your page
-        selector: '#chargify1',
-
-        // ot overrides default label
-        label: 'FIRST NAME',
-
-        // it overrides default placeholder
-        placeholder: 'John',
-
-        // if a given field is optional by default, you can make it required
-        required: false,
-
-        // it overrides default error message
-        message: 'First name is not valid. Please update it.',
-
-        // it overrides or defines max length
-        maxlength: '30',
-
-        // it overrides global styles for this field only
-        style: {
-          field: {
-            backgroundColor: '#ffdfdf',
-            padding: '3px',
-            borderRadius: '5px'
-          },
-          input: {
-            backgroundColor: '#fdfde1',
-            paddingTop: '2px',
-            paddingBottom: '1px',
-            placeholder: { color: 'green' }
-          },
-          label: {
-            paddingTop: '2px',
-            paddingBottom: '1px',
-            fontSize: '11px'
-          },
-          message: { paddingTop: '2px', paddingBottom: '1px' }
-        }
-      },
-
-      lastName: {
-        selector: '#chargify1',
-        label: 'LAST NAME',
-        placeholder: 'Doe',
-        required: false,
-        message: 'This field is not valid. Please update it.',
-        maxlength: '30',
-        style: {
-          field: {
-            backgroundColor: '#ffdfdf',
-            padding: '3px',
-            borderRadius: '5px'
-          },
-          input: {
-            backgroundColor: '#fdfde1',
-            paddingTop: '2px',
-            paddingBottom: '1px'
-          },
-          label: {
-            paddingTop: '2px',
-            paddingBottom: '1px',
-            fontSize: '11px'
-          },
-          message: { paddingTop: '2px', paddingBottom: '1px' }
-        }
-      }
-
-
-    });
-  }
-
-  componentDidUpdate(prevProps) {  
-    if (prevProps.type !== this.props.type) {
-      this.chargify.load({type: this.props.type});
-      this.setState({token: ''});
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.chargifyForm = React.createRef();
+        this.state = {token: ''};
     }
-  }
 
-  componentWillUnmount() {
-    this.chargify.unload();
-  }
+    handleSubmit(e) {
+        e.preventDefault();
 
-  render() {
-    return(
-      <form onSubmit={this.handleSubmit} ref={this.chargifyForm}>
-        <div id="chargify-form">
-          <div className="host-field" id={"chargify1"}>
-            <input className="host-input" data-chargify="firstName" value="Jane"/>
-            <input className="host-input" data-chargify="lastName" value="Mehmed"/>
-          </div>
+        this.chargify.token(
+            this.chargifyForm.current,
 
-          <div className="host-field">
-            <input className="host-input" data-chargify="address" value="123 Main St"/>
-          </div>
-          <div className="host-field">
-            <input className="host-input" data-chargify="address2" value="#321"/>
-          </div>
-          <div className="host-field">
-            <input className="host-input" data-chargify="city" value="San Antonio"/>
-          </div>
-          <div className="host-field">
-            <input className="host-input" data-chargify="state" value="TX"/>
-          </div>
-          <div className="host-field">
-            <input className="host-input" data-chargify="country" value="US"/>
-          </div>
-          <div className="host-field">
-            <input className="host-input" data-chargify="zip" value="12345"/>
-          </div>
-        </div>
-        <label>
-          Hidden Token: <input id="host-token" disabled value={this.state.token}/>
-        </label>
-        <p>
-          <button type="submit">Submit Host Form</button>
-        </p>
-      </form>
-    )
-  }
+            (token) => {
+                console.log('{host} token SUCCESS - token: ', token);
+                this.setState({token: token});
+            },
+
+            (error) => {
+                console.log('{host} token ERROR - err: ', error);
+            }
+        );
+    }
+
+    componentDidMount() {
+        this.chargify = new window.Chargify();
+
+        this.chargify.load({
+            // selector where the iframe will be included in the host's HTML (i.e. '#chargify-form')
+            // optional if you have a `selector` on each and every field
+            selector: '#chargify-form',
+
+          // (i.e. '1a2cdsdn3lkn54lnlkn')
+            publicKey: '{API-KEY}',
+
+          // form type (possible values: 'card' or 'bank')
+            type: this.state.type || 'card',
+
+          // points to your Chargify site
+            serverHost: 'https://{SOME-DOMAIN}.chargify.com',
+
+          // optional label/translation (i.e. '(optional field)') for optional fields
+          // Especially useful if you use 'fields' option
+            optionalLabel: '(optional field)',
+
+
+            // required label/translation (i.e. '*') for required fields
+            // Especially useful if you use 'fields' option
+            requiredLabel: '*',
+
+            firstName: {
+                // selector, where the iframe with this field will be included
+                // on your page
+                selector: '#chargify1',
+
+                // ot overrides default label
+                label: 'FIRST NAME',
+
+                // it overrides default placeholder
+                placeholder: 'John',
+
+                // if a given field is optional by default, you can make it required
+                required: false,
+
+                // it overrides default error message
+                message: 'First name is not valid. Please update it.',
+
+                // it overrides or defines max length
+                maxlength: '30',
+
+                // it overrides global styles for this field only
+                style: {
+                    field: {
+                        backgroundColor: '#ffdfdf',
+                        padding: '3px',
+                        borderRadius: '5px'
+                    },
+                    input: {
+                        backgroundColor: '#fdfde1',
+                        paddingTop: '2px',
+                        paddingBottom: '1px',
+                        placeholder: {color: 'green'}
+                    },
+                    label: {
+                        paddingTop: '2px',
+                        paddingBottom: '1px',
+                        fontSize: '11px'
+                    },
+                    message: {paddingTop: '2px', paddingBottom: '1px'}
+                }
+            },
+
+            lastName: {
+                selector: '#chargify1',
+                label: 'LAST NAME',
+                placeholder: 'Doe',
+                required: false,
+                message: 'This field is not valid. Please update it.',
+                maxlength: '30',
+                style: {
+                    field: {
+                        backgroundColor: '#ffdfdf',
+                        padding: '3px',
+                        borderRadius: '5px'
+                    },
+                    input: {
+                        backgroundColor: '#fdfde1',
+                        paddingTop: '2px',
+                        paddingBottom: '1px'
+                    },
+                    label: {
+                        paddingTop: '2px',
+                        paddingBottom: '1px',
+                        fontSize: '11px'
+                    },
+                    message: {paddingTop: '2px', paddingBottom: '1px'}
+                }
+            }
+
+
+        });
+    }
+
+    componentDidUpdate(prevProps) {
+        if (prevProps.type !== this.props.type) {
+            this.chargify.load({type: this.props.type});
+            this.setState({token: ''});
+        }
+    }
+
+    componentWillUnmount() {
+        this.chargify.unload();
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit} ref={this.chargifyForm}>
+                <div id="chargify-form">
+                    <div className="host-field" id={"chargify1"}>
+                        <input className="host-input" data-chargify="firstName" value="Jane"/>
+                        <input className="host-input" data-chargify="lastName" value="Mehmed"/>
+                    </div>
+
+                    <div className="host-field">
+                        <input className="host-input" data-chargify="address" value="123 Main St"/>
+                    </div>
+                    <div className="host-field">
+                        <input className="host-input" data-chargify="address2" value="#321"/>
+                    </div>
+                    <div className="host-field">
+                        <input className="host-input" data-chargify="city" value="San Antonio"/>
+                    </div>
+                    <div className="host-field">
+                        <input className="host-input" data-chargify="state" value="TX"/>
+                    </div>
+                    <div className="host-field">
+                        <input className="host-input" data-chargify="country" value="US"/>
+                    </div>
+                    <div className="host-field">
+                        <input className="host-input" data-chargify="zip" value="12345"/>
+                    </div>
+                </div>
+                <label>
+                    Hidden Token: <input id="host-token" disabled value={this.state.token}/>
+                </label>
+                <p>
+                    <button type="submit">Submit Host Form</button>
+                </p>
+            </form>
+        )
+    }
 }
 
 export default ChargifyForm;
